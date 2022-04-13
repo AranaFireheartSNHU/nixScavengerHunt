837033074e58c8b3918cf55c493c9a31449f4ab2
Jack <jcanducci@gmail.com>
2022-04-13 20:20:30 +0000

    Created user and ssh key setup

--- createUser.sh
+++ createUser.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-USERDIR=/home/septri/users
+USERDIR=/home/septri/nixScavengerHunt/users
 
 if [[ ! -d $USERDIR ]];
 then
 	mkdir $USERDIR
 fi
 
-ssh-keygen -b 2048 -t rsa -f "/home/septri/users/test" -N ""
+STUDENTS=./studentNames.txt
+
+while read -r line; do
+    #echo "adduser $line"
+    LOWERCASE=${line,,}
+    echo $LOWERCASE
+    sudo adduser -q --disabled-password $LOWERCASE
+    echo "y"
+    sudo mkdir /home/$LOWERCASE/.ssh
+    sudo chown septri /home/$LOWERCASE/.ssh
+    ssh-keygen -b 2048 -t rsa -f "/home/septri/nixScavengerHunt/users/$LOWERCASE.pem" -N "" -m PEM
+    mv /home/septri/nixScavengerHunt/users/$LOWERCASE.pem.pub /home/$LOWERCASE/.ssh/authorized_keys
+    sudo chown -R $LOWERCASE /home/$LOWERCASE/.ssh
+done <$STUDENTS
+
+clear
+
+echo "Users all created. Pem files are in $USERDIR"
